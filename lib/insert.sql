9742f14d8d4fde0c7ba95af5fa7cb4525bc09457
Richard Wasonga <rowwasonga@gmail.com>
2025-05-16 20:49:43 +0300

    complete lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr.Chocolate', 19, 'M', 'black', 'assertive', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 20, 'M', 'grey', 'aggressive', false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 14, 'F', 'golden', 'passive', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 28, 'M', 'brown', 'assertive', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 30, 'F', 'white', 'neutral', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 31, 'M', 'red', 'shy', false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 28, 'F', 'black', 'empatehetic', false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 26, 'F', 'brown', 'shy', true);
