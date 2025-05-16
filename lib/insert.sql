276fdd9cae2b7357bab44c9664dd2df50c5fb9bf
Richard Wasonga <rowwasonga@gmail.com>
2025-05-16 22:50:52 +0300

    complete lab

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr.Chocolate', 19, 'M', 'black', 'assertive', true);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 20, 'M', 'grey', 'aggressive', false);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 14, 'F', 'golden', 'passive', true);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 28, 'M', 'brown', 'assertive', true);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 30, 'F', 'white', 'neutral', true);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 31, 'M', 'red', 'shy', false);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 28, 'F', 'black', 'empatehetic', false);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 26, 'F', 'brown', 'shy', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr.Chocolate', 19, 'M', 'black', 'assertive', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 20, 'M', 'grey', 'aggressive', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 14, 'F', 'golden', 'passive', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 28, 'M', 'brown', 'assertive', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 30, 'F', 'white', 'neutral', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 31, 'M', 'red', 'shy', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 28, 'F', 'black', 'empatehetic', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 26, 'F', 'brown', 'shy', 1);
